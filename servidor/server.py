68d6a5b7108cbc1953ad339a684a8f8b741e6e16
AlexanderVQ <villafuertequispealex@gmail.com>
2025-05-31 03:14:59 +0000

    Servidor simple inicial

---
+++ servidor/server.py
@@ -0,0 +1,21 @@
+from concurrent import futures
+
+import grpc
+import procesador_pb2
+import procesador_pb2_grpc
+
+class Servidor(procesador_pb2_grpc.ProcesadorImagenServicer):
+    def ProcesarImagen(self, request, context):
+        return procesador_pb2.ImagenReply("imagen procesada correctamente")
+
+def serve():
+    puerto = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    procesador_pb2_grpc.add_ProcesadorImagenServicer_to_server(Servidor(), server)
+    server.add_insecure_port("[::]:" + puerto)
+    server.start()
+    print("Server iniciado, escuchando en " + puerto)
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
