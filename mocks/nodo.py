03f60525c355e094e4e8423b10bd37ec13674aec
AlexanderVQ <villafuertequispealex@gmail.com>
2025-05-31 23:42:52 +0000

    Se convierte el nodo para que se comunique usando gRPC

--- mocks/nodo.py
+++ mocks/nodo.py
@@ -1,27 +1,37 @@
-from flask import Flask, request, jsonify, send_file
+from pathlib import Path
+import sys
 import io
-import cv2
 import numpy as np
+import cv2
 
-app = Flask(__name__)
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-@app.route("/procesar-nodo", methods=["POST"])
-def procesar_imagen_nodo():
-    archivo_imagen = request.files["img"]
-    data = archivo_imagen.read()
-    imagen_np = np.frombuffer(data, dtype=np.uint8)
-    img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
-    
-    # Se puede realizar otro procesamiento pero por el momento solo se convierte a escala de grises
-    img_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, buf = cv2.imencode(".png", img_gris)
-    resultado_bytes = buf.tobytes()
+from concurrent import futures
 
-    return send_file(
-        io.BytesIO(resultado_bytes),
-        mimetype="image/png",
-        as_attachment=False
-    )
+import grpc
+from proto import procesador_pb2
+from proto import procesador_pb2_grpc
+
+class Nodo(procesador_pb2_grpc.ProcesadorImagenServicer):
+    def ProcesarImagen(self, request, context):
+        imagen_np = np.frombuffer(request.data, dtype=np.uint8)
+        img = cv2.imdecode(imagen_np, cv2.IMREAD_COLOR)
+
+        # Se puede realizar otro procesamiento pero por el momento solo se convierte a escala de grises
+        img_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, buf = cv2.imencode(".png", img_gris)
+        resultado_bytes = buf.tobytes()
+
+        return procesador_pb2.ImagenReply(status="ok", imagen_data=resultado_bytes)
+
+def serve():
+    puerto = "50052"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    procesador_pb2_grpc.add_ProcesadorImagenServicer_to_server(Nodo(), server)
+    server.add_insecure_port("[::]:" + puerto)
+    server.start()
+    print("Nodo iniciado, escuchando en " + puerto)
+    server.wait_for_termination()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    serve()
