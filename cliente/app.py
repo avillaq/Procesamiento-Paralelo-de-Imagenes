0188b74d8950a2e8f65e0466d4e17e4fd4b35b1b
AlexanderVQ <villafuertequispealex@gmail.com>
2025-05-31 02:13:58 +0000

    ruta de prueba

--- cliente/app.py
+++ cliente/app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/procesar", methods=["POST"])
+def procesar_imagen():
+    print("Prueba")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
