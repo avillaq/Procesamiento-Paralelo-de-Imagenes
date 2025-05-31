dbcc1fc215d2df074dd10ec9a845a2174e5ba7a3
AlexanderVQ <villafuertequispealex@gmail.com>
2025-05-31 03:40:44 +0000

    Modificacion en el cliente  para enviar los datos al servidor

--- cliente/app.py
+++ cliente/app.py
@@ -1,4 +1,12 @@
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from flask import Flask, render_template, request
+
+import grpc
+from proto import procesador_pb2
+from proto import procesador_pb2_grpc
 
 app = Flask(__name__)
 
@@ -10,6 +18,12 @@ def index():
 def procesar_imagen():
     archivo_imagen = request.files["img"]
     data = archivo_imagen.read()
+
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = procesador_pb2_grpc.ProcesadorImagenStub(channel)
+        response = stub.ProcesarImagen(procesador_pb2.ImagenRequest(data=data))
+        print(f"Respuesta del servidor: {response}")
+
     print(f"Nombre: {archivo_imagen.filename}")
     return render_template("index.html")
 
