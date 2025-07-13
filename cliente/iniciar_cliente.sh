212d02ef8d5724d9e8412a5e6b4f1418ac67f869
Alexander VQ <91032255+avillaq@users.noreply.github.com>
2025-07-13 00:20:07 +0000

    Elimina los mensajes de estado de montaje en iniciar_cliente.sh para simplificar la salida del script.

--- cliente/iniciar_cliente.sh
+++ cliente/iniciar_cliente.sh
@@ -4,7 +4,4 @@ mkdir -p /mnt/almacenamiento_dist
 sleep 30
 
 mount -t glusterfs gluster1:gvol /mnt/almacenamiento_dist
-echo "GlusterFS montado en cliente:"
-ls -la /mnt/almacenamiento_dist
-
 exec python app.py
